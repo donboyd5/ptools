cff7761d95490f3407d36431545bbf9bf93a0ef9
donboyd5 <donboyd5@gmail.com>
2014-06-02 16:24:47 -0400

    initial commit

---
+++ R/ptools_financialfunctions.r
@@ -0,0 +1,45 @@
+# ptools_financialfunctions.r
+# Don Boyd
+# 6/2/2014
+
+
+#' @title Present value of an annuity-immediate (with pmt at end of period)
+#'
+#' @description \code{pvann} present value of annuity-immediate
+#' @usage pvann(i, n, pmt)
+#' @param i interest rate
+#' @param n number of periods
+#' @param p periodic payment
+#' @details Returns a POSITIVE present value for positive payment
+#' @return The initial payment
+#' @keywords pvann
+#' @export
+#' @examples
+#' pvann(.05, 30, 100000)
+pvann<-function(i, n, pmt){
+  # present value of an annuity-immediate (with pmt at end of period)
+  if(i==0) pv<-n*pmt else pv<-pmt*((1-(1+i)^(-n))/i)
+  return(pv)
+}
+
+#' @title Graduated annuity initial payment (payments grow at constant annual rate).
+#'
+#' @description \code{gaip} initial payment for a graduated annuity growing at rate g
+#' @usage gaip(p, i, g, n)
+#' @param p principal
+#' @param i interest rate
+#' @param g growth rate of payments (annual)
+#' @param n number of periods
+#' @details Returns POSITIVE payment for positive principal
+#' @return The initial payment in the graduated annuity
+#' @keywords gaip
+#' @export
+#' @examples
+#' gaip(450, .07, .03, 20)
+gaip<-function(p, i, g, n){
+  # returns POSITIVE payment for positive principal 
+  imgc<-(1+i)/(1+g)-1 # i minus g, compounded
+  gaf<- (1+i)/(1-pvann(imgc, n-1, -1)) # grad annuity factor - note "minus" pvann to adjust sign vs Excel
+  return(gaf*p)
+}
+
---
+++ inst/ptools_RPackageCreation.r
@@ -0,0 +1,90 @@
+# ptools_RPackageCreation.r
+# Don Boyd
+# 6/1/2014
+
+# http://www.molecularecologist.com/2013/11/using-github-with-r-and-rstudio/
+# https://www.rstudio.com/ide/docs/version_control/overview
+# http://kbroman.github.io/github_tutorial/
+
+# http://jeromyanglim.tumblr.com/post/63159223332/how-to-push-existing-git-rstudio-repository-to-github
+
+# from RStudio/Tools/Shell... execute the following to set the link to github up:
+#   git remote add origin https://github.com/donboyd5/ptools.git
+#   git push ptools
+
+# http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+# http://adv-r.had.co.nz/Package-basics.html
+# https://github.com/klutometis/roxygen#roxygen2
+# http://www.rstudio.com/ide/docs/packages/overview
+
+# http://cran.r-project.org/doc/manuals/R-exts.html#Data-in-packages
+# https://sites.google.com/site/hackoutwiki/developers-corner/developing-r-packages
+
+# The following SSH key was added to your account:
+#   
+#   GitHub for Windows - Don-PC
+# 75:4b:6d:82:41:88:79:28:e1:b2:7a:01:66:93:61:8f
+
+# 
+# If you believe this key was added in error, you can remove the key and disable
+# access at the following location:
+#   
+#   https://github.com/settings/ssh
+
+# getOption("defaultPackages")
+
+# http://rstudio-pubs-static.s3.amazonaws.com/628_79ab370204dd4b8a85ebbcde8ef95998.html
+
+# install.packages("devtools")
+# find_rtools()
+# devtools::install_github("klutometis/roxygen")
+# devtools::install_github("yihui/roxygen2") # I installed from CRAN
+
+library(devtools)
+library(roxygen2)
+
+setwd("E:\\R\\BoydPackages\\")
+# create("btools") only run this when needed
+
+# then, create the functions and get into the R subdirectory for the package
+
+
+# REPEAT the steps below each time the package is revised
+# documentation (must be in the r files with functions)
+setwd("./ptools")
+document()
+
+getwd()
+# install
+setwd("..")
+install("ptools")
+
+
+# optionally install from github
+# setwd("..")
+install_github("ptools","donboyd5")
+
+
+
+
+# remove.packages("btools")
+
+
+# here is a good example of how to document a function
+
+#' @title Sum of Vector Elements
+#'
+#' @description
+#' \code{sum} returns the sum of all the values present in its arguments.
+#'
+#' @details
+#' This is a generic function: methods can be defined for it directly
+#' or via the \code{Summary} group generic.  For this to work properly,
+#' the arguments \code{...} should be unnamed, and dispatch is on the
+#' first argument.'
+sum <- function(..., na.rm = TRUE) {}
+
+
+
+
+
