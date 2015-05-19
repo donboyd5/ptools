71ae9ad3fe714828391b7d195844a89791eabc9c
donboyd5 <donboyd5@gmail.com>
2015-05-19 10:09:31 -0400

    Added most of Yimeng's functions other than NC

---
+++ R/ptools_annuity_functions.r
@@ -0,0 +1,243 @@
+
+
+
+#' @title Calculate temporary annuity values from age x to retirment age (fixed end)
+#'
+#' @description \code{get_tla} present value of annuity-immediate
+#' @usage get_tla(px, i, scale, cashflow)
+#' @param px a vector of composite survivial probs from age x to x + n - 1. Length = n
+#' @param i discount rate, scalar
+#' @param scale numeric vector of length px
+#' @param cashflow The cashflow of the annuity can be specified by this argument. This is useful when calculating PV of benefits with COLA.
+#' @details  Suppose the age corresponding to px runs from a1 to aN, and f = aN + 1 (eg. age 30:64, f = 65)
+#' The function computes a..{x, f - x} and s_a..{x, f - x}, x runing from a1 to aN.
+#' The length of px is f - a1 
+#' Note that the last element is redundant, just used as a place holder. 
+
+#' @return an n vector storing the value of temporary life annuities from age x to age x + n - 1
+#' @keywords get_tla
+#' @export
+#' @examples
+#' get_tla(rep(0.98, 55), 0.08)
+get_tla <- function(px, i, scale = rep(1, length(px)), cashflow = rep(1, length(px))){
+  # suppose the age corresponding to px runs from a1 to aN, and f = aN + 1 (eg. age 30:64, f = 65)
+  # The function computes a..{x, f - x} and s_a..{x, f - x}, x runing from a1 to aN. 
+  # The length of px is f - a1 
+  # Note that the last element is redundant, just used as a place holder. 
+  
+  # inputs:
+  # px: an vector of composite survivial probs from age x to x + n - 1. Length = n
+  # i:  discount rate, scalar
+  # scale: how the annuity scale up over time. eg: 
+  #        1) salary scale. default is a n vector of 1, meaning no salary scale. used when calculating career based annuity
+  #        2) simple COLA scale: COLA increasing at a fixed percentage very year.  
+  # cashflow: The cashflow of the annuity can be specified by this argument. This is useful when calculating PV of benefits with COLA.
+  # output:
+  # tla: an n vector storing the value of temporary life annuities from age x to age x + n - 1.
+  tla <- numeric(length(px))
+  n <- length(tla)
+  
+  for(j in 1:n){
+    v   <- 1/(1 + i)^(0:(n - j)) # dicount vector
+    if(j < n) pxr <- cumprod(c(1, px[j:(n - 1)])) else pxr = 1      # survival probability to retirment at age x. Note that participant always survives at the beginning of age x
+    SS  <- scale[j:n]/scale[j]                # scale
+    tla[j] = sum(SS * v * pxr)                # computing annuity value at j
+  } 
+  return(tla)
+}
+
+
+#' @title Calculate temporary annuity values from a fixed entry age y to x (fixed start)
+#'
+#' @description \code{get_tla2} present value of annuity-immediate
+#' @usage get_tla2(px, i, sx)
+#' @param px a vector of composite survivial probs from age x to x + n - 1. Length = n
+#' @param i discount rate, scalar
+#' @param sx salary scale. default is an n vector of 1, meaning no salary scale. 
+#' @details  Suppose the age corresponding to px runs from a1 to aN, y = a1 (eg. age 30:65, y = 30)
+#' This function conputes a..{y, x - y} and s_a..{y, x - y}, x ruuning from a1 to aN. 
+#'
+#' Note that when x = a1 = y, we define a..{y, 0} = 0. so the first element is always 0. 
+#' For age x > y, the number of years receiviing annuity is x - y, the resulting annuity value will be placed at age x. 
+#' eg1: x = 31, y = 30,  annuity ($1) received only once at age 30, but the resulting annuity value will be placed at age 31.
+#' eg2: x = 65, y = 30, annuity received from age 30 to 64(total 65 years), the resulting annuity value will be placed at age 65
+#' 
+#' Note that the last 2 survival rates and last salary scale are redundant in the calculation, they are just used as place holders. 
+#' calculating the value of annuity running from 30 to 64 only involves survival rate from 30 to 63, 
+#' because the last annuity payment is paid at the begining of 64. )
+#' @return an n vector storing the value of temporary life annuities from age x to age x + n - 1.
+#' @keywords get_tla2
+#' @export
+#' @examples
+#' get_tla2(rep(0.98, 65), 0.08, rep(1.1, 65))
+get_tla2 = function(px, i, sx = rep(1, length(px))){
+  # Suppose the age corresponding to px runs from a1 to aN, y = a1 (eg. age 30:65, y = 30)
+  # This function conputes a..{y, x - y} and s_a..{y, x - y}, x ruuning from a1 to aN. 
+  
+  # Note that when x = a1 = y, we define a..{y, 0} = 0. so the first element is always 0. 
+  # For age x > y, the number of years receiviing annuity is x - y, the resulting annuity value will be placed at age x. 
+  # eg1: x = 31, y = 30,  annuity ($1) received only once at age 30, but the resulting annuity value will be placed at age 31.
+  # eg2: x = 65, y = 30, annuity received from age 30 to 64(total 65 years), the resulting annuity value will be placed at age 65
+  # Note that the last 2 survival rates and last salary scale are redundant in the calculation, they are just used as place holders. 
+  #(calculating the value of annuity running from 30 to 64 only involves survival rate from 30 to 63, 
+  # because the last annuity payment is paid at the begining of 64. )
+  
+  # inputs:
+  # px: an vector of composite survivial probs from age x to x + n - 1. Length = n. The minimum length of px allowed is 2. 
+  # i:  discount rate, scalar
+  # sx: salary scale. default is an n vector of 1, meaning no salary scale. 
+  # output:
+  # tla: an n vector storing the value of temporary life annuities from age x to age x + n - 1.
+  tla = numeric(length(px))
+  n = length(tla)
+  
+  # tla[1] will be kept as 0, next calculate tla[2:n]:
+  for(j in 1:(n - 1)){
+    v   <- 1/(1 + i)^(0:(j - 1))                                  # dicount vector
+    if(j == 1) pxr <- 1 else pxr <- cumprod(c(1, px[1:(j - 1)]))  # survival probability to retirment at age x. Note that participant always survives at the beginning of age x
+    SS  <- sx[1:j]/sx[1]                                          # salary scale
+    tla[j + 1] = sum(SS * v * pxr)                                # computing annuity value at j;
+  } 
+  return(tla) 
+}
+
+
+#' @title Calculate temporary annuity values from a fixed entry age y to x (fixed start), simpler implementation
+#'
+#' @description \code{get_tla2a} present value of annuity-immediate
+#' @usage get_tla2a(px, i, sx)
+#' @param px a vector of composite survivial probs from age x to x + n - 1. Length = n
+#' @param i discount rate, scalar
+#' @param sx salary scale. default is an n vector of 1, meaning no salary scale. 
+#' @details  Suppose the age corresponding to px runs from a1 to aN, y = a1 (eg. age 30:65, y = 30)
+#' This function conputes a..{y, x - y} and s_a..{y, x - y}, x ruuning from a1 to aN. 
+
+#' Note that when x = a1 = y, we define a..{y, 0} = 0. so the first element is always 0. 
+#' For age x > y, the number of years receiviing annuity is x - y, the resulting annuity value will be placed at age x. 
+#' eg1: x = 31, y = 30,  annuity ($1) received only once at age 30, but the resulting annuity value will be placed at age 31.
+#' eg2: x = 65, y = 30, annuity received from age 30 to 64(total 65 years), the resulting annuity value will be placed at age 65
+#' Note that the last 2 survival rates and last salary scale are redundant in the calculation, they are just used as place holders. 
+#' calculating the value of annuity running from 30 to 64 only involves survival rate from 30 to 63, 
+#' because the last annuity payment is paid at the begining of 64. )
+
+#' @return an n vector storing the value of temporary life annuities from age x to age x + n - 1.
+#' @keywords get_tla2a
+#' @export
+#' @examples
+#' get_tla2a(rep(0.98, 65), 0.08, rep(1.1, 65))
+#' get_tla2(rep(0.98, 65), 0.08, rep(1.1, 65))
+get_tla2a <- function(px, i, sx = rep(1, length(px))){
+  
+  n <- length(px)
+  tla <- numeric(n)
+  v <- 1/(1 + i)
+  
+  tla[-1] <- cumsum(cumprod(c(1,px[1:(n-2)])* v * sx[1:(n - 1)]/sx[1])/v)
+  
+  return(tla)   
+}
+
+
+
+# # 1.3 PVFB of term costs
+# #' @title Present value of an annuity-immediate (with pmt at end of period)
+# #'
+# #' @description \code{pvann} present value of annuity-immediate
+# #' @usage pvann(i, n, pmt)
+# #' @param i interest rate
+# #' @param n number of periods
+# #' @param p periodic payment
+# #' @details Returns a POSITIVE present value for positive payment
+# #' @return The initial payment
+# #' @keywords pvann
+# #' @export
+# #' @examples
+# #' pvann(.05, 30, 100000)
+# get_PVFB <- function(px, v, TC){ # present values of subsets of TC (fixed end)
+#   # This function compute the total present value of TC[j:n] at the beginning of time j, with j running from 1 to n. 
+#   # The function can be used to calculate PVFB of term costs of ancillary benefits or retirement benefits with multiple
+#   # retirement ages. 
+#   
+#   # Inputs
+#   # px: numeric vector of length n. Probability of survival at time 1 through n
+#   # v : numeric. discount factor 1/(1 + i)
+#   # TC: numeric vector of length n. A series of term costs. Term costs are valued at the begninning of period. 
+#   # Returns
+#   # PVFBs of fixed end contracting windows of TC. 
+#   
+#   n <- length(px)
+#   
+#   PVFB <- sapply(seq_len(n), function(j) ifelse(j == n, TC[j], sum(cumprod(c(1, (px[j:(n - 1)] * v))) * TC[j:n])))
+#   
+#   return(PVFB)
+# }
+# 
+# # 1.4 NC of UC and PUC
+# #' @title Present value of an annuity-immediate (with pmt at end of period)
+# #'
+# #' @description \code{pvann} present value of annuity-immediate
+# #' @usage pvann(i, n, pmt)
+# #' @param i interest rate
+# #' @param n number of periods
+# #' @param p periodic payment
+# #' @details Returns a POSITIVE present value for positive payment
+# #' @return The initial payment
+# #' @keywords pvann
+# #' @export
+# #' @examples
+# #' pvann(.05, 30, 100000)
+# get_NC.UC <- function(px, v, TC){
+#   # This function is a variation of get_PVFB. It is used to calculate NC under UC and PUC methods.
+#   # Below we explain the major difference between get_NC.UC and get_PVFB:
+#   # 1. Why TC[(j + 1):n]?  Remember NC is the discounted value of benefit accrual. During age x, the individual can 
+#   #    accrue benefit for age x + 1 to r'', so the corresponding elements in TC are TC[(j + 1):n]. Note that 
+#   #    TC[j+1] is gx.r(j+1)*qxr(j+1)*ax(j+1) in PUC. 
+#   # 2. Why start discounting from the 1st element? Since at j the individual starts accruing benefit from j + 1, 
+#   #    we need to discount the value in j + 1.  
+#   # Note The last elements (at age r'') of the result is NA by construction. 
+#   # px must be survival probability from min(age) to r''.
+#   # TC must be defined as  
+#   #  UC for retirement:    gx.r(x) * qxr(x) * ax(x), x running from y (entry age) to r'' (eg. 20 to 65 in Winklevoss book)
+#   #  PUC for retirement:   Bx(x)/(x - y) * gx.r(x) * qxr(x) * ax(x), x running from entry age (y) to r'' (0 when x = y)      
+#   #  PUC for vested terms: Bx(x)/(x - y) * gx.r(x) * qxt.a * lead(pxRm) * v^(r.max - age) * ax[age == r.max]
+#   n <- length(px) # n is r''
+#   
+#   Fun_NC <- function(j) ifelse(j == n, NA, sum(cumprod(px[j:(n - 1)]) * v^(1:(n-j)) * TC[(j + 1):n]))
+#   
+#   NC <- sapply(seq_len(n), Fun_NC)
+#   
+#   return(NC)
+# }
+# 
+# # 1.5 AL of PUC
+# #' @title Present value of an annuity-immediate (with pmt at end of period)
+# #'
+# #' @description \code{pvann} present value of annuity-immediate
+# #' @usage pvann(i, n, pmt)
+# #' @param i interest rate
+# #' @param n number of periods
+# #' @param p periodic payment
+# #' @details Returns a POSITIVE present value for positive payment
+# #' @return The initial payment
+# #' @keywords pvann
+# #' @export
+# #' @examples
+# #' pvann(.05, 30, 100000)
+# get_AL.PUC <- function(px, v, TC){
+#   # This function is a variation of get_PVFB. It is used to calculate AL under PUC methods.
+#   
+#   # Note that the only difference between get_AL.PUC and get_PVFB is that TC[j] is multiplied by (j - 1)
+#   
+#   # Note that y(entry age) corresponds to index 1 and age x corresponds to index j, so at age x the individual
+#   # has been accruing benefits for x - y years, which is equal to j - 1 years. (eg. Assuming y = 20, then when x = 21 and j = 2 the 
+#   # individual have accrued benefits for 1 year (x - y = 21 - 20 and j - 1 =  2 - 1).
+#   
+#   # TC must be defined the same way as in get_NC.UC. 
+#   # the first element (age y) should be zero, the last element should be the same as the last element in TC.
+#   
+#   n <- length(px) # n is r'' - y + 1
+#   AL <- sapply(seq_len(n), 
+#                function(j) ifelse(j == n, TC[j]*(j-1), sum(cumprod(c(1, (px[j:(n - 1)] * v))) * TC[j:n] * (j - 1)))
+#   )
+#   return(AL)
+# }
--- R/ptools_financialfunctions.r
+++ R/ptools_financialfunctions.r
@@ -1,47 +1,126 @@
 # ptools_financialfunctions.r
 # Don Boyd
-# 6/3/2014
+# 5/19/2015
 
 
-#' @title Present value of an annuity-immediate (with pmt at end of period)
+#' @title Constant dollar amortization method
 #'
-#' @description \code{pvann} present value of annuity-immediate
-#' @usage pvann(i, n, pmt)
+#' @description \code{amort_cd} initial payment for a graduated annuity growing at rate g
+#' @usage amort_cd(p, i, m, end)
+#' @param p principal
 #' @param i interest rate
-#' @param n number of periods
-#' @param p periodic payment
-#' @details Returns a POSITIVE present value for positive payment
-#' @return The initial payment
-#' @keywords pvann
+#' @param m number of periods
+#' @param end if TRUE, payment at the end of period
+#' @details Returns POSITIVE payment for positive principal 
+#' @return The amortization payment
+#' @keywords amort_cd
 #' @export
 #' @examples
-#' pvann(.05, 30, 100000)
-pvann <- function(i, n, pmt) {
-  # present value of an annuity-immediate (with pmt at end of period)
-  if(i==0) pv <- n*pmt else pv<-pmt*((1-(1+i)^(-n))/i)
-  return(pv)
+#' amort_cd(100, 0.08, 10, FALSE)
+amort_cd <- function(p, i, m, end = FALSE) rep(pmt(p, i, m, end), m)
+
+
+#' @title Constant percent amortization method
+#'
+#' @description \code{amort_cp} stream of payments for a graduated annuity growing at rate g
+#' @usage amort_cp(p, i, m, g, end)
+#' @param p principal
+#' @param i interest rate
+#' @param m number of periods
+#' @param g growth rate
+#' @param end if TRUE, payment at the end of period
+#' @details Returns POSITIVE payment for positive principal 
+#' @return The amortization payment
+#' @keywords amort_cp
+#' @export
+#' @examples
+#' amort_cp(100, 0.08, 10, .03, FALSE)
+amort_cp <- function(p, i, m, g, end = FALSE) gaip(p, i, m, g, end)*(g + 1)^(1:m - 1)
+
+
+#' @title Constant percent amortization method
+#'
+#' @description \code{amort_sl} stream of payments for straight-line payments
+#' @usage amort_sl(p, i, m, end)
+#' @param p principal
+#' @param i interest rate
+#' @param m number of periods
+#' @param end if TRUE, payment at the end of period
+#' @details Returns POSITIVE payment for positive principal 
+#' @return The amortization payment
+#' @keywords amort_sl
+#' @export
+#' @examples
+#' amort_sl(100, 0.08, 10, FALSE)
+#' 
+#' amort_cd(100, 0.08, 10, FALSE)
+#' amort_cp(100, 0.08, 10, 0.05, FALSE)
+#' amort_sl(100, 0.08, 10, FALSE)
+amort_sl <- function(p, i, m, end = FALSE){
+  # # Straight line amortization method
+  # See Winklevoss(1993, p101)
+  if(end){
+    sl <- i*(p - p*(0:(m - 1))/m) + p/m
+  } else {
+    d <- 1/(1+i)
+    sl <- d*(p - p*(1:m)/m) + p/m}
+  return(sl)
+}
+
+
+#' @title Function for choosing amortization methods
+#'
+#' @description \code{amort_LG} stream of payments for straight-line payments
+#' @usage amort_LG(p, i, m, g, end, method)
+#' @param p principal
+#' @param i interest rate
+#' @param m number of periods
+#' @param g growth rate
+#' @param end if TRUE, payment at the end of period
+#' @param method one of c("cd", "cp", "sl")
+#' @details Returns stream of payments
+#' @return The stream of payments
+#' @keywords amort_LG
+#' @export
+#' @examples
+#' amort_LG(100, 0.08, 10, .03, method="cd")
+#' amort_LG(100, 0.08, 10, .03, method="cp")
+#' amort_LG(100, 0.08, 10, .03, method="sl")
+amort_LG <- function(p, i, m, g, end = FALSE, method = "cd"){
+  # amortize the gain/loss using specified amortization method
+  switch(method,
+         cd = amort_cd(p, i ,m, end),
+         cp = amort_cp(p, i, m, g, end),
+         sl = amort_sl(p, i, m, end)
+  )
 }
 
 
 #' @title Graduated annuity initial payment (payments grow at constant annual rate).
 #'
 #' @description \code{gaip} initial payment for a graduated annuity growing at rate g
-#' @usage gaip(p, i, g, n)
+#' @usage gaip(p, i, n, g, end)
 #' @param p principal
 #' @param i interest rate
-#' @param g growth rate of payments (annual)
 #' @param n number of periods
+#' @param g growth rate of payments (annual)
+#' @param end if TRUE, payment at the end of period
 #' @details Returns POSITIVE payment for positive principal 
 #' @return The initial payment in the graduated annuity
 #' @keywords gaip
 #' @export
 #' @examples
-#' gaip(450, .07, .03, 20)
-gaip <- function(p, i, g, n) {
-  # returns POSITIVE payment for positive principal 
-  imgc <- (1+i)/(1+g)-1 # i minus g, compounded
-  gaf <- (1+i)/(1-pvann(imgc, n-1, -1)) # grad annuity factor - note "minus" pvann to adjust sign vs Excel
-  return(gaf*p)
+#' gaip(100, 0.08, 10, 0.04)
+#' gaip(100, 0.08, 10, 0.02, end = TRUE)
+gaip <- function(p, i, n, g, end = FALSE){
+  # p=principal, i=interest rate, n=periods, g=growth rate in payments
+  # calculating gaip directly
+  # end: , if TRUE, payment at the end of period. 
+  if(end) p <- p*(1 + i) 
+  k <- (1 + g)/(1 + i)
+  a_sn <- (1 - k^n )/(1 - k)
+  pmt <- p/a_sn
+  return(pmt)
 }
 
 
@@ -61,26 +140,49 @@ pdiff <- function(first, second) {
   return((first - second)/second*100)
 }
 
+
 #' @title Amortization function
 #'
 #' @description \code{pmt} level amortization for principal p, interest rate i, n periods
-#' @usage pmt(p, i, n)
+#' @usage pmt(p, i, n, end)
 #' @param p principal
 #' @param i interest rate
 #' @param n number of periods
+#' @param end if true, payment at end of period, otherwise beginning
 #' @details Returns POSITIVE payment for positive principal 
 #' @return The level payment in the annuity
 #' @keywords pmt
 #' @export
 #' @examples
-#' pmt(450, .07, 20)
-pmt <- function(p, i, n) { 
-  # amortization function
-  # p=principal, i=interest rate, n=periods
-  pmt <- p * (1+i)^n * i/((1+i)^n - 1)
-  return(pmt)
+#' pmt(100, 0.08, 10)
+#' pmt(100, 0.08, 10, TRUE)
+pmt <- function(p, i, n, end = FALSE){
+  # amortization function with constant payment at each period 
+  # p = principal, i = interest rate, n = periods. 
+  # end: , if TRUE, payment at the end of period. 
+  if(end) p <- p*(1 + i)
+  a_n <- (1 - (1 + i)^(-n))/(1 - 1/(1 + i))
+  pmt <- p / a_n
+  return(pmt)  
 }
 
 
-
+#' @title Present value of an annuity-immediate (with pmt at end of period)
+#'
+#' @description \code{pvann} present value of annuity-immediate
+#' @usage pvann(i, n, pmt)
+#' @param i interest rate
+#' @param n number of periods
+#' @param pmt periodic payment
+#' @details Returns a POSITIVE present value for positive payment
+#' @return The initial payment
+#' @keywords pvann
+#' @export
+#' @examples
+#' pvann(.05, 30, 100)
+pvann <- function(i, n, pmt) {
+  # present value of an annuity-immediate (with pmt at end of period)
+  if(i==0) pv <- n*pmt else pv <- pmt * ((1-(1+i) ^ (-n)) / i)
+  return(pv)
+}
 
